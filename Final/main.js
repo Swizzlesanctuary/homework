ee977785f966552e22d32457a3670d19396e637d
Swizzlesanctuary <gvantsajishkariani0@gmail.com>
2025-09-28 17:38:05 +0200

    add links to index logos

--- Final/main.js
+++ Final/main.js
@@ -51,7 +51,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form");
 
-    form.addEventListener("submit", (e) => {
+    form?.addEventListener("submit", (e) => {
         e.preventDefault(); 
         window.location.href = "index.html"; 
     });
