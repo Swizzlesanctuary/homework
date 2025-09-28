9b3eb71e5c610050dfdd1772c9f63f192590e713
Swizzlesanctuary <gvantsajishkariani0@gmail.com>
2025-09-28 15:37:38 +0200

    add id to hamburder

---
+++ Final/main.js
@@ -0,0 +1,21 @@
+let modal = document.getElementById("hamburger");
+let openBtn = document.getElementById("open");  
+
+let closeBtn = document.getElementById("close"); 
+let result = document.getElementById("result");
+let plus = document.getElementById("plus");
+
+let count = 0;
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+plus.addEventListener("click", () => {
+  count++;  
+  result.innerText = count; 
+});
